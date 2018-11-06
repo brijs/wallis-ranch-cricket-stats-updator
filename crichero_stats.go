d0e849a719206b6103655a0bc2bd4cde38c6b1cb
Brijesh Shetty <brshetty@linkedin.com>
2018-11-05 19:58:04 -0800

    Update with go fmt

--- crichero_stats.go
+++ crichero_stats.go
@@ -1,159 +1,157 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "net/http"
-    "time"
-    "sync"
-    "os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type PlayerID uint
 
 type PlayerProfile struct {
-    ID       PlayerID
-    Name     string
-    PhotoURL string
-    Batting BattingStat
-    Bowling BowlingStat
+	ID       PlayerID
+	Name     string
+	PhotoURL string
+	Batting  BattingStat
+	Bowling  BowlingStat
 }
 
 type ApiResponseData struct {
-    Players []PlayerProfile
-    Batting []BattingStat
-    Bowling []BowlingStat
+	Players []PlayerProfile
+	Batting []BattingStat
+	Bowling []BowlingStat
 }
 
 type BattingStat struct {
-    Matches    uint
-    Innings    uint
-    NotOuts    uint
-    Runs       uint
-    Highest    uint
-    Average    float32
-    StrikeRate float32
-    Fours      uint
-    Sixes      uint
-    Ducks      uint
-    Thirties   uint
-    Fifties    uint
-    Hundreds   uint
+	Matches    uint
+	Innings    uint
+	NotOuts    uint
+	Runs       uint
+	Highest    uint
+	Average    float32
+	StrikeRate float32
+	Fours      uint
+	Sixes      uint
+	Ducks      uint
+	Thirties   uint
+	Fifties    uint
+	Hundreds   uint
 }
 
 type BowlingStat struct {
-    Matches uint
-    Innings uint
-    Overs float32
-    Runs uint
-    Maidens uint
-    Wickets uint
-    Economy float32
-    StrikeRate float32
-    Average float32
-    Wides uint
-    NoBalls uint
-    DotBalls uint
-    BestBowling string
-
+	Matches     uint
+	Innings     uint
+	Overs       float32
+	Runs        uint
+	Maidens     uint
+	Wickets     uint
+	Economy     float32
+	StrikeRate  float32
+	Average     float32
+	Wides       uint
+	NoBalls     uint
+	DotBalls    uint
+	BestBowling string
 }
 
 type ApiResponse struct {
-    Success bool
-    Data    ApiResponseData
+	Success bool
+	Data    ApiResponseData
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getWrapAPIKey() (string) {
-    val, ok := os.LookupEnv("BRIJ_WRAP_API_KEY")
-    if !ok {
-        log.Fatalln("BRIJ_WRAP_API_KEY is not set")
-        return val
-    } else {
-        return val
-    }
+func getWrapAPIKey() string {
+	val, ok := os.LookupEnv("BRIJ_WRAP_API_KEY")
+	if !ok {
+		log.Fatalln("BRIJ_WRAP_API_KEY is not set")
+		return val
+	} else {
+		return val
+	}
 }
 
-func GetWRPlayers() ([]PlayerProfile){
-    WRPlayersURL := fmt.Sprintf("https://wrapapi.com/use/brij/tests/WRCricPlayers/latest?wrapAPIKey=%s", getWrapAPIKey())
-    r, err := myClient.Get(WRPlayersURL)
-    defer r.Body.Close()
-    if err != nil {
-        log.Fatalln(err)
-    }
+func GetWRPlayers() []PlayerProfile {
+	WRPlayersURL := fmt.Sprintf("https://wrapapi.com/use/brij/tests/WRCricPlayers/latest?wrapAPIKey=%s", getWrapAPIKey())
+	r, err := myClient.Get(WRPlayersURL)
+	defer r.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    if r.StatusCode != http.StatusOK {
-        log.Fatalln("Received Bad HTTP Status:", r.StatusCode)
-    }
+	if r.StatusCode != http.StatusOK {
+		log.Fatalln("Received Bad HTTP Status:", r.StatusCode)
+	}
 
-    bodyBytes, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        log.Fatalln(err)
-    }
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    resp := &ApiResponse{}
-    err = json.Unmarshal(bodyBytes, resp)
-    if err != nil {
-        fmt.Println(err)
-    }
-    if resp.Success != true {
-        log.Fatalf("Got Response.success=false %+v", resp)
-    }
-    if resp.Data.Players == nil {
-        log.Fatalf("Got Nil/Empty players %+v", resp)
-    }
+	resp := &ApiResponse{}
+	err = json.Unmarshal(bodyBytes, resp)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if resp.Success != true {
+		log.Fatalf("Got Response.success=false %+v", resp)
+	}
+	if resp.Data.Players == nil {
+		log.Fatalf("Got Nil/Empty players %+v", resp)
+	}
 
-    return resp.Data.Players
+	return resp.Data.Players
 
 }
 
 func GetPlayerStats(playerStatJobs <-chan PlayerProfile, outputProfiles chan<- PlayerProfile, wg *sync.WaitGroup) {
-        for p := range playerStatJobs {
-            log.Printf("GetPlayerStats: processing ID=%s\n", p.Name)
-            WRPlayerStatsURL := fmt.Sprintf("https://wrapapi.com/use/brij/tests/WRCricPlayerStat/latest?player_id=%d&wrapAPIKey=%s", p.ID, getWrapAPIKey())
-            r, err := myClient.Get(WRPlayerStatsURL)
-            defer r.Body.Close()
-            if err != nil {
-                log.Fatalln(err)
-            }
+	for p := range playerStatJobs {
+		log.Printf("GetPlayerStats: processing ID=%s\n", p.Name)
+		WRPlayerStatsURL := fmt.Sprintf("https://wrapapi.com/use/brij/tests/WRCricPlayerStat/latest?player_id=%d&wrapAPIKey=%s", p.ID, getWrapAPIKey())
+		r, err := myClient.Get(WRPlayerStatsURL)
+		defer r.Body.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-            if r.StatusCode != http.StatusOK {
-                log.Fatalln("Received Bad HTTP Status:", r.StatusCode)
-            }
+		if r.StatusCode != http.StatusOK {
+			log.Fatalln("Received Bad HTTP Status:", r.StatusCode)
+		}
 
-            bodyBytes, err := ioutil.ReadAll(r.Body)
-            if err != nil {
-                log.Fatalln(err)
-            }
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-            resp := &ApiResponse{}
-            err = json.Unmarshal(bodyBytes, resp)
-            if err != nil {
-                fmt.Println(err)
-            }
-            // if resp.Success != true {
-            //     log.Fatalf("Got Response.success=false %+v for ID=%d Name=%s", resp, p.ID, p.Name)
-            // }
-            // if resp.Data.Batting == nil || resp.Data.Bowling == nil {
-            //     log.Fatalf("Got Nil Batting/Bowling stat %+v", resp)
-            // }
+		resp := &ApiResponse{}
+		err = json.Unmarshal(bodyBytes, resp)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// if resp.Success != true {
+		//     log.Fatalf("Got Response.success=false %+v for ID=%d Name=%s", resp, p.ID, p.Name)
+		// }
+		// if resp.Data.Batting == nil || resp.Data.Bowling == nil {
+		//     log.Fatalf("Got Nil Batting/Bowling stat %+v", resp)
+		// }
 
-            if len(resp.Data.Batting) > 0 {
-                p.Batting = resp.Data.Batting[0]
-            }
-            if len(resp.Data.Bowling) > 0 {
-                p.Bowling = resp.Data.Bowling[0]
-            }
+		if len(resp.Data.Batting) > 0 {
+			p.Batting = resp.Data.Batting[0]
+		}
+		if len(resp.Data.Bowling) > 0 {
+			p.Bowling = resp.Data.Bowling[0]
+		}
 
-            // log.Printf("GetPlayerStats: Adding ID=%+v\n", p)
-            outputProfiles <- p
-        }
+		// log.Printf("GetPlayerStats: Adding ID=%+v\n", p)
+		outputProfiles <- p
+	}
 
-        wg.Done()
-        return
+	wg.Done()
+	return
 
 }
-
--- main.go
+++ main.go
@@ -2,53 +2,53 @@ package main
 
 import (
 	"log"
-    "sync"
+	"sync"
 )
 
 func main() {
 	log.Printf("Main: Begin WR Stats update...\n")
 
-    playerStatJobs := make(chan PlayerProfile, 1000)
-    outputProfileJobs := make(chan PlayerProfile, 1000)
+	playerStatJobs := make(chan PlayerProfile, 1000)
+	outputProfileJobs := make(chan PlayerProfile, 1000)
 
-    // threads to get stats
-    const NUM_THREADS = 1
-    wg := new(sync.WaitGroup)
-    wg.Add(NUM_THREADS)
-    for i := 0; i < NUM_THREADS; i++ {
-       log.Printf("Main: Starting thread %d to get stats\n", i)
-       go GetPlayerStats(playerStatJobs, outputProfileJobs, wg)
-    }
+	// threads to get stats
+	const NUM_THREADS = 1
+	wg := new(sync.WaitGroup)
+	wg.Add(NUM_THREADS)
+	for i := 0; i < NUM_THREADS; i++ {
+		log.Printf("Main: Starting thread %d to get stats\n", i)
+		go GetPlayerStats(playerStatJobs, outputProfileJobs, wg)
+	}
 
-    // get list of players
-    const MAX_PLAYERS = 500
-    playerProfiles := GetWRPlayers()
-    i := 0
-    for _, p := range playerProfiles {
-        if i >= MAX_PLAYERS {break}
-        log.Printf("Main: Adding profile %s to get stats\n", p.Name)
-        playerStatJobs <- p
-        i++
-    }
-    close(playerStatJobs)
+	// get list of players
+	const MAX_PLAYERS = 500
+	playerProfiles := GetWRPlayers()
+	i := 0
+	for _, p := range playerProfiles {
+		if i >= MAX_PLAYERS {
+			break
+		}
+		log.Printf("Main: Adding profile %s to get stats\n", p.Name)
+		playerStatJobs <- p
+		i++
+	}
+	close(playerStatJobs)
 
-    // wait for
-    log.Printf("Main: Waiting for jobs to finish")
-    wg.Wait()
-    log.Printf("Main: Done Waiting for jobs to finish")
-    close(outputProfileJobs)
+	// wait for
+	log.Printf("Main: Waiting for jobs to finish")
+	wg.Wait()
+	log.Printf("Main: Done Waiting for jobs to finish")
+	close(outputProfileJobs)
 
-    // collect output
-    var outputProfiles []PlayerProfile
-    for o := range outputProfileJobs {
-        outputProfiles = append(outputProfiles, o)
-        log.Printf("Main: Output => %s", o.Name)
-    }
+	// collect output
+	var outputProfiles []PlayerProfile
+	for o := range outputProfileJobs {
+		outputProfiles = append(outputProfiles, o)
+		log.Printf("Main: Output => %s", o.Name)
+	}
 
-
-    // Update sheets
-    PersistBattingStats(outputProfiles)
-    PersistBowlingStats(outputProfiles)
-    log.Printf("Main: Done")
+	// Update sheets
+	PersistBattingStats(outputProfiles)
+	PersistBowlingStats(outputProfiles)
+	log.Printf("Main: Done")
 }
-
